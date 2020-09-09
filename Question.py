c2b3b0bde4c9e9dde08853a1dfdf9822a40756b2
Akshika23 <68143308+Akshika23@users.noreply.github.com>
2020-09-09 20:03:50 +0530

    Create Question.py

---
+++ Question.py
@@ -0,0 +1,11 @@
+list1 =[12,-7,5,64,-14]
+print(list1);
+for i in list1:
+    if i>0:
+        print(i,end=",");
+list2 = [12,14,-95,3]
+print('\n')
+print(list2);
+for j in list2:
+    if j>0:
+        print(j,end=",");
